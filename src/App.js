77936665bbedf320377417094d97dfb815a78216
Saikoyomii9 <saikoyomii9@gmail.com>
2023-02-23 23:48:13 -0500

    Version 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React from "react";
+import Countdown from "react-countdown";
+
+const Completionist = () => <span> You are good to go! </span>
+
+const renderer = ({hours, minutes, seconds, completed}) => {
+  if (completed) {
+    return <Completionist />
+  } else {
+    return <span> {hours}:{minutes}:{seconds}</span>
+  }
+};
 
 function App() {
   return (
     <div>
       <h2>Countdown</h2>
+      <div>
+        <Countdown
+        data={Date.now() + 10000}
+        renderer={renderer}
+        >
+          <Completionist />
+        </Countdown>
+      </div>
     </div>
-   
   );
 }
 
